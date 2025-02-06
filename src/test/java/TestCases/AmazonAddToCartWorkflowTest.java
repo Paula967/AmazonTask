7729d64d50727f79e04f786ad7b617a5ed670f65
Paula <paula.farid9@gmail.com>
2025-02-06 22:59:32 +0200

    sixth

--- src/test/java/TestCases/AmazonAddToCartWorkflowTest.java
+++ src/test/java/TestCases/AmazonAddToCartWorkflowTest.java
@@ -12,10 +12,15 @@ import java.util.Objects;
 public class AmazonAddToCartWorkflowTest extends BaseClass {
     @Test
     public void AddToCartTest(){
-        DetailsItemPage detailsItemPage= homePage.SearchFor("iPad").ClickOnSearchButton().SortingItemsBy("Price: High to Low").ClickOnAnySearchResult(1);
+        DetailsItemPage detailsItemPage= homePage
+                                                 .SearchFor("iPad")
+                                                 .ClickOnSearchButton()
+                                                 .SortingItemsBy("Price: High to Low")
+                                                 .ClickOnAnySearchResult(1);
         String ProductName=detailsItemPage.getProductName();
         Double ProductPrice=detailsItemPage.getProductPrice();
         ShoppingCartPage shoppingCartPage=detailsItemPage.ClickOnAddToCart().ClickOnMyCartButton();
-        Assert.assertTrue(ProductName.contains(shoppingCartPage.getProductInfo()) && Objects.equals(ProductPrice, shoppingCartPage.getProductPrice()),"No Matched Data");
+        Assert.assertTrue(ProductName.contains(shoppingCartPage.getProductInfo())
+                && Objects.equals(ProductPrice, shoppingCartPage.getProductPrice()),"No Matched Data");
     }
 }
