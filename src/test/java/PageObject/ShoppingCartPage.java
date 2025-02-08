50208027bedce4fb0834af4726d8dbb70dda8ff8
paula.farid9@gmail.com <paula.farid9@gmail.com>
2025-02-08 04:17:10 +0200

    Update ShoppingCartPage.java

--- src/test/java/PageObject/ShoppingCartPage.java
+++ src/test/java/PageObject/ShoppingCartPage.java
@@ -9,14 +9,14 @@ public class ShoppingCartPage extends BasePage{
     }
 
     //Locators
-    private final By cartButton=By.xpath("//a[@href='/-/en/cart?ref_=sw_gtc']");
+    private final By goToBasektButton=By.xpath("//a[@href='/-/en/cart?ref_=sw_gtc']");
     private final By productInfoInCart =By.xpath("//span[@class='a-truncate-cut']");
     private final By productPriceInCart =By.xpath("//span[@class='a-price a-text-price sc-product-price sc-white-space-nowrap a-size-medium']" +
             "/span[2]");
 
     //Actions
     public void clickGoToBasket(){
-        clickElement(cartButton);
+        clickElement(goToBasketButton);
     }
     public String getCartProductName(){
         String data = getTextFromElement(productInfoInCart);
